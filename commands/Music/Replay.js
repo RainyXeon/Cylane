f8ba7070f74d33b584968ef7b998a5e72e0e3f18
XeonE52680v3 <minh15052008@gmail.com>
2022-10-27 12:16:34 +0700

    Add replay command

---
+++ commands/Music/Replay.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+
+// Main code
+module.exports = { 
+    name: "replay",
+    description: "Replay the current song!",
+    run: async (interaction, client, language) => {
+        await interaction.deferReply({ ephemeral: false });
+        const msg = await interaction.editReply(`${client.i18n.get(language, "music", "replay_loading")}`);
+
+        const player = client.manager.players.get(interaction.guild.id);
+        if (!player) return msg.edit(`${client.i18n.get(language, "noplayer", "no_player")}`);
+        const { channel } = interaction.member.voice;
+        if (!channel || interaction.member.voice.channel !== interaction.guild.members.me.voice.channel) return msg.edit(`${client.i18n.get(language, "noplayer", "no_voice")}`);
+      
+        await player.send({ op: "seek", guildId: interaction.guild.id, position: 0 });
+
+        const embed = new EmbedBuilder()
+            .setDescription(`${client.i18n.get(language, "music", "replay_msg")}`)
+            .setColor(client.color);
+
+        msg.edit({ content: " ", embeds: [embed] });
+    }
+};
