1869f2a5d74b908010d42c8d260714e31f39d6f5
XeonE52680v3 <minh15052008@gmail.com>
2023-01-07 19:39:47 +0700

    Add pause status in ưebsocket

--- src/websocket/status.loop.js
+++ src/websocket/status.loop.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: "pause_status",
+  name: "loop_status",
   run: async (client, json, ws) => {
     const player = client.manager.players.get(json.guild)
     if (!player) return ws.send(JSON.stringify({ error: "0x100", message: "No player on this guild" }))
-    return ws.send(JSON.stringify({ op: player.paused ? 3 : 4, guild: player.guildId }))
+    return ws.send(JSON.stringify({ op: player.loop === "none" ? 9 : 8, guild: player.guildId }))
   }
 }
