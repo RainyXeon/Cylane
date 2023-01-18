0b6084a9aa09eee1a76de11e84dda369f424edcd
XeonE52680v3 <minh15052008@gmail.com>
2023-01-18 17:21:52 +0700

    Update seek command

--- src/commands/Music/Seek.js
+++ src/commands/Music/Seek.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
 const formatDuration = require('../../structures/FormatDuration.js');
+const time_regex = /(^[0-9][\d]{0,3}):(0[0-9]{1}$|[1-5]{1}[0-9])/
 
 // Main code
 module.exports = { 
@@ -8,15 +9,27 @@ module.exports = {
     categories: "Music",
     options: [
         {
-            name: "seconds",
-            description: "The number of seconds to seek the timestamp by.",
-            type: ApplicationCommandOptionType.Number,
+            name: "time",
+            description: "Set the position of the playing track. Example: 0:10 or 120:10",
+            type: ApplicationCommandOptionType.String,
             required: true,
         }
     ],
     run: async (interaction, client, language) => {
-        await interaction.deferReply({ ephemeral: false });
-        const value = interaction.options.getNumber("seconds");
+        await interaction.deferReply({ ephemeral: false })
+        let value
+        const time = interaction.options.getString("time");
+
+        console.log(time_regex.test(time), time.split(/:/))
+        if (!time_regex.test(time)) return interaction.editReply(`${client.i18n.get(language, "music", "seek_invalid")}`)
+        else {
+            const [m, s] = time.split(/:/)
+            const min = Number(m) * 60
+            const sec = Number(s)
+            value = min + sec
+            console.log(value)
+        }
+
         const msg = await interaction.editReply(`${client.i18n.get(language, "music", "seek_loading")}`);
         
         const player = client.manager.players.get(interaction.guild.id);
@@ -25,11 +38,18 @@ module.exports = {
         if (!channel || interaction.member.voice.channel !== interaction.guild.members.me.voice.channel) return msg.edit(`${client.i18n.get(language, "noplayer", "no_voice")}`);
 
         if(value * 1000 >= player.queue.current.length || value < 0) return msg.edit(`${client.i18n.get(language, "music", "seek_beyond")}`);
-        await player.send({ op: "seek", guildId: interaction.guild.id, position: value * 1000 });
+        await player.seek(value * 1000);
 
         const song_position = player.shoukaku.position
 
-        const Duration = formatDuration(song_position);
+        let final_res
+
+        if (song_position < (value * 1000)) final_res = song_position + (value * 1000)
+        else final_res = value * 1000
+
+        console.log(final_res / 1000)
+
+        const Duration = formatDuration(final_res);
 
         const seeked = new EmbedBuilder()
             .setDescription(`${client.i18n.get(language, "music", "seek_msg", {
